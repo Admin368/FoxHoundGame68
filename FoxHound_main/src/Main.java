bfb312e725187f7b78f8006c6a7e3b12bcb3c75b
Admin368 <58757588+Admin368@users.noreply.github.com>
2021-01-02 20:47:25 +0800

    test-scanners

--- FoxHound_main/src/Main.java
+++ FoxHound_main/src/Main.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(test.addxm("buy pizza"));
-//        FoxHoundGame.main();
+        String buffer = "Empty";
+//        Xprint(XgetChar());
+        array();
+    }
+
+    public static void Xprint(String content){
+        System.out.println(content);
+    }
+    public static String XgetLine(){
+        String line;
+        System.out.println("Please Enter Input:");
+        Scanner input = new Scanner(System.in);
+        line = input.nextLine()+"!";
+        return line;
+    }
+    public static String XgetChar(){
+        String charX;
+        System.out.println("Please Enter Input:");
+        Scanner input = new Scanner(System.in);
+        charX = input.findInLine("p");
+        return charX;
+    }
+    public static void array(){
+        String[] arrayX = {"s","d"};
+        arrayX[0] = "a";
+        System.out.println(arrayX[0]);
     }
 }
